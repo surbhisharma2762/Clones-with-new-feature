1e7d02b5c3f20e8bf7d476b35750cbff9c7eca76
surbhisharma2762 <surbhisharma1581@gmail.com>
2024-11-05 11:50:09 +0530

    updating library

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// alert("running your spotify")
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/Clones-with-new-feature/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("td");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  return songs;
+}
+async function main() {
+  let songs = await getSongs();
+  console.log(songs);
+  let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li>${song.replaceAll("%20"," ")}</li>`;
+  }
+  //play the first song
+  var audio = new Audio(songs[0]);
+  audio.play();
+}
+main()
